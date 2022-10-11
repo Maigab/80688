7d0a087db89be856f0e2e9f0606fc2d90b8c88ee
Maigab <mailenegab@gmail.com>
2022-10-11 16:50:29 -0500

    sistemas

---
+++ practica05/objeto.js
@@ -0,0 +1,16 @@
+var o = {
+    prop:"existo"
+}
+
+var o2 = new Object();
+o.prop = 'existe';
+
+var msj_bot = {
+    saludo : "hola",
+    despedida : "adios",
+    preocupacion : "como estas?",
+    sorpresa : "orale"
+}
+
+var arr_msj = ["hola", "adios", "como estas?", "orale"]
+console.log()
